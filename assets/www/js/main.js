c33728302aaedb2695a892e00ec6a52032a782d5
Yama Farooq <yama@thisisthebox.co.uk>
2013-04-16 15:15:51 +0100

    fixed on device ready function

---
+++ assets/www/js/main.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright: Yama Ghulam Farooq. 2013.
+ * All rights reserved.
+*/
+console.log("Loading Main.js");
+$("#home_pg #locbtn").live("click", function() {
+  console.log("onError called");
+alert("ddddd");
+});
